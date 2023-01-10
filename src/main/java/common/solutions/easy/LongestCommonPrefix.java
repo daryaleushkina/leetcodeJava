3093e4abbbc19166f5a479acb3cda9730eff2cc1
darya_leushkina <?,tmRAZL$E4=+R>
2023-01-10 20:11:30 +0400

    added a second solution to problem 14

--- src/main/java/common/solutions/easy/LongestCommonPrefix.java
+++ src/main/java/common/solutions/easy/LongestCommonPrefix.java
@@ -2,8 +2,8 @@ package common.solutions.easy;
 
 public class LongestCommonPrefix {
 
-//  Task №14  https://leetcode.com/problems/longest-common-prefix/
-    public static String longestCommonPrefix(String[] strs) {
+    //  Task №14  https://leetcode.com/problems/longest-common-prefix/
+    public static String longestCommonPrefix_1(String[] strs) {
         StringBuilder prefix = new StringBuilder();
         StringBuilder temp = new StringBuilder();
 
@@ -21,6 +21,28 @@ public class LongestCommonPrefix {
             if (flag)
                 prefix.append(strs[0].charAt(i));
             else return String.valueOf(prefix);
+        }
+
+        return String.valueOf(prefix);
+    }
+
+    public static String longestCommonPrefix_2(String[] strs) {
+        StringBuilder prefix = new StringBuilder(strs[0]);
+
+        boolean flag = false;
+        while (!flag) {
+            int lastIndex = 0;
+            for (int i = 0; i < strs.length; i++) {
+                int index = strs[i].indexOf(String.valueOf(prefix));
+                if (index == -1 || index > 0) {
+                    prefix.deleteCharAt(prefix.length() - 1);
+                    if (String.valueOf(prefix) == "")
+                        return String.valueOf(prefix);
+                    break;
+                }
+                lastIndex ++;
+            }
+            flag = lastIndex == strs.length;
         }
 
         return String.valueOf(prefix);
