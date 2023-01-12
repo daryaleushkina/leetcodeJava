aec14a49204ccfd2e6c42b89735a31f025c6a7f6
darya_leushkina <?,tmRAZL$E4=+R>
2023-01-12 09:10:01 +0400

    fix solution for task 12

--- src/main/java/common/solutions/medium/IntegerToRoman.java
+++ src/main/java/common/solutions/medium/IntegerToRoman.java
@@ -1,60 +1,36 @@
 package common.solutions.medium;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class IntegerToRoman {
 
-//    Task №12 https://leetcode.com/problems/integer-to-roman
+    //    Task №12 https://leetcode.com/problems/integer-to-roman
     public String intToRoman(int num) {
+        String[] roman = new String[]{"M", "D", "C", "L", "X", "V", "I",};
+        int[] nums = new int[]{1000, 500, 100, 50, 10, 5, 1};
+
         String s = "";
-        s = s + "M".repeat(num / 1000);
-        if (num % 1000 >= 900) {
-            s = s + "CM";
-            num -= 900;
+        for (int i = 0; i < nums.length; i++) {
+            int count = num / nums[i];
+            num -= count * nums[i];
+            s = s + roman[i].repeat(count);
+
+            switch (roman[i]) {
+                case "C": case "M": case "X":
+                    if (num % nums[i] >= nums[i] - nums[i + 2]) {
+                        s += roman[i + 2] + roman[i];
+                        num -= nums[i] - nums[i + 2];
+                    }
+                    break;
+                case "D": case "L": case "V":
+                    if (num % nums[i] >= nums[i] - nums[i + 1]) {
+                        s += roman[i + 1] + roman[i];
+                        num -= nums[i] - nums[i + 1];
+                    }
+                    break;
+            }
         }
-        num = num - ((num / 1000) * 1000);
-
-        s = s + "D".repeat(num / 500);
-        if (num % 500 >= 400) {
-            s = s + "CD";
-            num -= 400;
-        }
-        num = num - ((num / 500) * 500);
-
-        s = s + "C".repeat(num / 100);
-        if (num % 100 >= 90) {
-            s = s + "XC";
-            num -= 90;
-        }
-        num = num - ((num / 100) * 100);
-
-
-        s = s + "L".repeat(num / 50);
-        if (num % 50 >= 40) {
-            s = s + "XL";
-            num -= 40;
-
-        }
-        num = num - ((num / 50) * 50);
-
-        s = s + "X".repeat(num / 10);
-        if (num % 10 == 9) {
-            s = s + "IX";
-            num -= 9;
-        }
-        num = num - ((num / 10) * 10);
-
-
-        s = s + "V".repeat(num / 5);
-        if (num % 5 == 4) {
-            s = s + "IV";
-            num -= 4;
-
-        }
-        num = num - ((num / 5) * 5);
-
-        s = s + "I".repeat(num);
-
         return s;
     }
 }
