ae379796c9c7e19b2639909282bb61bbbf5ab145
darya_leushkina <?,tmRAZL$E4=+R>
2023-08-14 18:17:58 +0400

    add task №2460. Remove Element

---
+++ src/main/java/common/solutions/easy/array/ApplyOperations.java
@@ -0,0 +1,30 @@
+package common.solutions.easy.array;
+
+import java.util.Arrays;
+
+public class ApplyOperations {
+    //    Task №2460. Remove Element https://leetcode.com/problems/apply-operations-to-an-array/
+
+    public int[] applyOperations(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int count = nums.length;
+        int i = 0;
+        while (i < count) {
+            if (nums[i] == 0) {
+                for (int j = i; j < nums.length - 1; j++)
+                    nums[j] = nums[j + 1];
+                nums[nums.length-1] = 0;
+                count--;
+            } else {
+                i++;
+            }
+        }
+       return nums;
+    }
+}
