fcfdfff1ae674f6d9d53aab545b3b1688e8d6ea8
darya_leushkina <?,tmRAZL$E4=+R>
2023-08-21 14:58:48 +0400

    add task №1346 Check If N and Its Double Exist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,45 @@
-import common.solutions.easy.array.DuplicateZeros;
-import common.solutions.easy.array.RemoveElement;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        RemoveElement array = new RemoveElement();
-        System.out.println(array.removeElement(new int[]{3,2,2,3}, 3));
-    }
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
+        for (int i = 0; i < n; i++) {
+            String data = scanner.next();
+            Set<Character> namesSet = new HashSet<>(); // или LinkedHashSet для сохранения порядка
+
+
+            String[] parts = data.split(",");
+
+            String lastName = parts[0];
+            String firstName = parts[1];
+            String middleName = parts[2];
+            for (char c : lastName.toCharArray()) {
+                namesSet.add(c);
+            }
+            for (char c : firstName.toCharArray()) {
+                namesSet.add(c);
+            }
+            for (char c : middleName.toCharArray()) {
+                namesSet.add(c);
+            }
+
+            int day = Integer.parseInt(parts[3]);
+            int month = Integer.parseInt(parts[4]);
+            int year = Integer.parseInt(parts[5]);
+
+            System.out.println(namesSet.size());
+            System.out.println(day % 10 + day / 10 + month % 10 + month / 10);
+            int ans = namesSet.size() + (day % 10 + day / 10 + month % 10 + month / 10) * 64 + (lastName.charAt(0) - 'A' + 1) * 256;
+
+            String hexNumber = Integer.toHexString(ans);
+
+            System.out.print(hexNumber.substring(hexNumber.length() - 3).toUpperCase() + " ");
+        }
+
+    }
 }
--- src/main/java/common/solutions/easy/array/CheckIfExist.java
+++ src/main/java/common/solutions/easy/array/CheckIfExist.java
@@ -1,16 +1,41 @@
 package common.solutions.easy.array;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class CheckIfExist {
     //    Task №1346. Check If N and Its Double Exist  https://leetcode.com/problems/check-if-n-and-its-double-exist/
 
 
-    public boolean checkIfExist(int[] arr) {
-        for (int i = 0; i<arr.length; i++) {
-            for (int j = i+1; j<arr.length; j++) {
-                if (arr[i] == arr[j]*2 || arr[i]*2==arr[j])
-                    return true;
-            }
+    public void checkIfExist(int[] arr) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String data = scanner.next();
+            Set<String> namesSet = new HashSet<>(); // или LinkedHashSet для сохранения порядка
+
+
+            String[] parts = data.split(",");
+
+            String lastName = parts[0];
+            String firstName = parts[1];
+            String middleName = parts[2];
+            namesSet.add(lastName);
+            namesSet.add(firstName);
+            namesSet.add(middleName);
+
+            int day = Integer.parseInt(parts[3]);
+            int month = Integer.parseInt(parts[4]);
+            int year = Integer.parseInt(parts[5]);
+
+            int ans = namesSet.size() + (day%10 + day/10 + month%10+month/10)*64 + (lastName.charAt(0) - 'A' + 1)*256;
+
+            String hexNumber = Integer.toHexString(ans);
+
+            System.out.print( hexNumber.substring(hexNumber.length() - 3).toUpperCase()+ " ");
+
         }
-        return false;
     }
 }
