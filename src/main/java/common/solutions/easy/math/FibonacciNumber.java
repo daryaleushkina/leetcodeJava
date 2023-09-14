5d516ca24e5cbe2400e41f51d06db9c0c20d1652
darya_leushkina <?,tmRAZL$E4=+R>
2023-09-14 16:36:29 +0400

    optimize task 509. Fibonacci Number

--- src/main/java/common/solutions/easy/math/FibonacciNumber.java
+++ src/main/java/common/solutions/easy/math/FibonacciNumber.java
@@ -4,9 +4,27 @@ public class FibonacciNumber {
 
     // Task 509. Fibonacci Number https://leetcode.com/problems/fibonacci-number
     public int fib(int n) {
-        if (n<=1)
-            return n;
-        else return fib(n-1) + fib(n-2);
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+        ans[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            ans[i] = ans[i - 1] + ans[i - 2];
+        }
+        return ans[n];
+
+        // First solution
+//        if (n<=1)
+//            return n;
+//        else return fib(n-1) + fib(n-2);
+
+
+        // Second solution
+
         // int prevValue1 = 0;
         // int prevValue2 = 1;
         // int ans = 0;
