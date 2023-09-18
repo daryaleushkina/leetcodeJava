8ed6e4d419d58825aad34be89fcd63000fe26397
darya_leushkina <?,tmRAZL$E4=+R>
2023-09-18 11:10:10 +0400

    add Task 125. Valid Palindrome

---
+++ src/main/java/common/solutions/easy/string/IsPalindrome.java
@@ -0,0 +1,27 @@
+package common.solutions.easy.string;
+
+public class IsPalindrome {
+    // Task 125. Valid Palindrome https://leetcode.com/problems/valid-palindrome/
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+
+            if (!Character.isLetterOrDigit(leftChar))
+                left++;
+            else if (!Character.isLetterOrDigit(rightChar))
+                right--;
+            else if (leftChar != rightChar)
+                return false;
+            else {
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+}
