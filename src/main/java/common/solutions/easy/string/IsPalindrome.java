5a996a18d110e57ebedf781434274c6a94ec36be
darya_leushkina <?,tmRAZL$E4=+R.>
2023-09-24 22:57:41 +0400

    add task 125. Valid Palindrome

--- src/main/java/common/solutions/easy/string/IsPalindrome.java
+++ src/main/java/common/solutions/easy/string/IsPalindrome.java
@@ -2,26 +2,39 @@ package common.solutions.easy.string;
 
 public class IsPalindrome {
     // Task 125. Valid Palindrome https://leetcode.com/problems/valid-palindrome/
-    public boolean isPalindrome(String s) {
+    public boolean validPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
-            char leftChar = Character.toLowerCase(s.charAt(left));
-            char rightChar = Character.toLowerCase(s.charAt(right));
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
 
-            if (!Character.isLetterOrDigit(leftChar))
-                left++;
-            else if (!Character.isLetterOrDigit(rightChar))
-                right--;
-            else if (leftChar != rightChar)
-                return false;
-            else {
+            if (leftChar != rightChar) {
+                String s1 = s.substring(0, left) + s.substring(left + 1, s.length());
+                String s2 = s.substring(0, right) + s.substring(right + 1, s.length());
+
+                return isPalindrome(s1) || isPalindrome(s2);
+            } else {
                 left++;
                 right--;
             }
         }
 
+        return true;
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
         return true;
     }
 }
