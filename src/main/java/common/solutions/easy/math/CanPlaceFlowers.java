aa984854f538f9bc4c656681d4bbd2a174d6f09e
darya_leushkina <?,tmRAZL$E4=+R.>
2023-09-25 19:10:38 +0400

    add task 605. Can Place Flowers

--- src/main/java/common/solutions/easy/math/CanPlaceFlowers.java
+++ src/main/java/common/solutions/easy/math/CanPlaceFlowers.java
@@ -6,27 +6,22 @@ public class CanPlaceFlowers {
     // Task 605. Can Place Flowers https://leetcode.com/problems/can-place-flowers/
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 0;
-        int tempCount = 0;
+
         for (int i = 0; i < flowerbed.length; i++) {
-            if (flowerbed[i] == 0)
-                tempCount++;
-            if (flowerbed[i] != 0 || i == flowerbed.length - 1)
-                if (tempCount > 0) {
-                    if (i == 0 || i == flowerbed.length - 1) {
-                        if (tempCount / 3 > 0)
-                            count += ((tempCount / 3) + 1);
-                        else count++;
-                    } else {
-                        count += (tempCount / 3);
-                        if (tempCount % 3 > 0)
-                            count++;
-                    }
-                    tempCount = 0;
+            if (flowerbed[i] == 0) {
+                boolean canPlace = true;
+
+                if (i > 0 && flowerbed[i - 1] == 1)
+                    canPlace = false;
+                if (i < flowerbed.length - 1 && flowerbed[i + 1] == 1)
+                    canPlace = false;
+
+                if (canPlace) {
+                    flowerbed[i] = 1;
+                    count++;
                 }
+            }
         }
-
-
-        count += tempCount / 3 + 1;
 
         return count >= n;
 
