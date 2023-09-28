7eb7019c2c726518dcfe0866a611385071c4fcb8
darya_leushkina <?,tmRAZL$E4=+R.>
2023-09-28 11:40:26 +0400

    add task 459. Repeated Substring Pattern

--- src/main/java/common/solutions/easy/string/RepeatedSubstringPattern.java
+++ src/main/java/common/solutions/easy/string/RepeatedSubstringPattern.java
@@ -3,7 +3,7 @@ package common.solutions.easy.string;
 public class RepeatedSubstringPattern {
     // Task 459. Repeated Substring Pattern https://leetcode.com/problems/repeated-substring-pattern/
     public boolean repeatedSubstringPattern(String s) {
-        String copy = s;
+        String copy = s + s;
         String ans = copy.substring(1, copy.length()-1);
         return ans.contains(s);
     }
