9755f64afebf8fc92919c9b5667004c1eaea3375
darya_leushkina <?,tmRAZL$E4=+R.>
2023-10-11 13:57:55 +0300

    improve task 977. Squares of a Sorted Array

--- src/main/java/common/solutions/easy/array/SquaresOfASortedArray.java
+++ src/main/java/common/solutions/easy/array/SquaresOfASortedArray.java
@@ -6,17 +6,28 @@ public class SquaresOfASortedArray {
     // Task 977. Squares of a Sorted Array https://leetcode.com/problems/squares-of-a-sorted-array/
 
     public int[] sortedSquares(int[] nums) {
-        int[] newNums = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            newNums[i] = nums[i] * nums[i];
+        short n = (short) nums.length;
+        int[] ans = new int[n];
+
+        short left = 0;
+        short right = (short) (n - 1);
+        short index = right;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                ans[index] = leftSquare;
+                left++;
+            } else {
+                ans[index] = rightSquare;
+                right--;
+            }
+
+            index--;
         }
 
-        // return Arrays.stream(newNums).sorted().toArray();
-        return newNums;
-    }
-
-    public int[] quickSort(int[] nums) {
-
-        return new int[]{2, 4};
+        return ans;
     }
 }
