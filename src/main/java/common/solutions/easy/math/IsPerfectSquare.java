aaffc6a07e7006b2bdc42abce5b2ed6671d79d98
darya_leushkina <?,tmRAZL$E4=+R.>
2024-02-06 02:55:05 +0400

    add task №367. Valid Perfect Square

---
+++ src/main/java/common/solutions/easy/math/IsPerfectSquare.java
@@ -0,0 +1,25 @@
+package common.solutions.easy.math;
+
+public class IsPerfectSquare {
+    // Task №367. Valid Perfect Square https://leetcode.com/problems/valid-perfect-square/description/
+    public boolean isPerfectSquare(int num) {
+        if (num == 1)
+            return true;
+        if (num < 4)
+            return false;
+
+        long left = 0;
+        long right = num;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long product = mid * mid;
+            if (product == num)
+                return true;
+            else if (product > num)
+                right = mid - 1;
+            else left = mid + 1;
+        }
+        return false;
+
+    }
+}
