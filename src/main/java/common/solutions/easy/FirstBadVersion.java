9426a48a80b6f39b9bc7d544fc5de2a753803cd2
darya_leushkina <?,tmRAZL$E4=+R.>
2024-02-06 13:08:08 +0400

    add task 278

--- src/main/java/common/solutions/easy/FirstBadVersion.java
+++ src/main/java/common/solutions/easy/FirstBadVersion.java
@@ -1,22 +1,26 @@
 package common.solutions.easy;
 
 public class FirstBadVersion {
+    int firstBad = 6;
     boolean isBadVersion(int version) {
-        return version % 2 == 0;
+        return version == firstBad;
     }
 
     public int firstBadVersion(int n) {
-        int current = n;
-        int first = -1;
-        boolean flag = true;
-        while (flag) {
-            if (isBadVersion(current)) {
-                first = current;
-                current--;
-            } else if (first == -1) {
-                current++;
-            } else break;
+        // Task 278. First Bad Version https://leetcode.com/problems/first-bad-version/
+        int left = 1;
+        int right = n;
+        int mid = left + (right - left) / 2;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (isBadVersion(mid))
+                if (!isBadVersion(mid - 1))
+                    return mid;
+                else right = mid - 1;
+            else left = mid + 1;
         }
-        return first;
+
+        return left;
     }
 }
